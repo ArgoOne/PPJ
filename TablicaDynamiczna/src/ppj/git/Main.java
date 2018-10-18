7aae096292bd052c4d7d1b0c7deb7309f1130c09
ArgoOne <argo_@op.pl>
2018-10-18 17:34:07 +0200

    Zadanie tablica

--- TablicaDynamiczna/src/ppj/git/Main.java
+++ TablicaDynamiczna/src/ppj/git/Main.java
@@ -5,18 +5,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Scanner in = new Scanner (System.in);
-    		System.out.print("Podaj wartosc n: ");
-    			int num1= (int) Double.parseDouble(in.next());
-	int i,j;
-	
-		for (i=1; i<=num1; i++)
-		{
-			for (j=0; j<i; j++)
-			{
-				System.out.print(j);
+
+		Scanner in = new Scanner(System.in);
+		System.out.print("Podaj wartosc n: ");
+		int num1 = (int) Double.parseDouble(in.next());
+
+		int tab[][] = new int[num1][0];
+		int licznik = 0;
+
+		for (int i = 0; i < num1; i++) {
+			tab[i] = new int[i + 1];
+		}
+		for (int i = 0; i < tab.length; i++) {
+			for (int j = 0; j < tab[i].length; j++) {
+				tab[i][j] = j;
 			}
+		}
+		for (int i = 0; i < tab.length; i++) {
+			for (int j = 0; j < tab[i].length; j++)
+				System.out.print(tab[i][j] + " ");
 			System.out.println();
 		}
 	}
