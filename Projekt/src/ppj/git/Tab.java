a04403c7c7472d5032b3cfe69be855f7ac64327e
ArgoOne <argo_@op.pl>
2019-01-15 15:12:17 +0100

    Update

--- Projekt/src/ppj/git/Tab.java
+++
@@ -1,8 +0,0 @@
-package ppj.git;
-public class Tab{
-	
-  public static void main(String[] args){
-
-			  System.out.println("Pierwszy projekt w GitHub");
-  }
-}
--- Wynagrodzenie/src/ppj/git/Main.java
+++ Wynagrodzenie/src/ppj/git/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		
 		Scanner in = new Scanner (System.in);
 		
-		System.out.print("Podaj iosc przeprowadzonych godzin:  ");
+		System.out.print("Podaj ilosc przepracownanych godzin:  ");
 		double godziny1 = Double.parseDouble(in.next());
 		
 		System.out.print("Podaj stawke:  ");
@@ -16,17 +16,17 @@ public class Main {
 		
 		if (godziny1<=40)
 		{
-			System.out.printf("Iloczyn=%8.2f",godziny1*stawka1);	
+			System.out.printf("Wynagrodzenie =%8.2f",godziny1*stawka1);	
 		}
 		
 		else if (godziny1>=60)
 		{
-			System.out.printf("Iloczyn=%8.2f",((40*stawka1)+(20*(stawka1*1.5))));
+			System.out.printf("Wynagrodzenie =%8.2f",((40*stawka1)+(20*(stawka1*1.5))));
 			System.out.println(" Maksymalnie 60 godzin pracy na tydzien ");
 			}
 		
 		else {
-			System.out.printf("Iloczyn=%8.2f",((40*stawka1)+((godziny1-40)*(stawka1*1.5))));
+			System.out.printf("Wynagrodzenie =%8.2f",((40*stawka1)+((godziny1-40)*(stawka1*1.5))));
 		}
 	}
 }
