b3a74e64ef9336241b6df15fc17eb6809f2f0d3c
ArgoOne <argo_@op.pl>
2019-01-28 22:13:50 +0100

    Update

--- TablicaDynamiczna/src/ppj/git/Main.java
+++ TablicaDynamiczna/src/ppj/git/Main.java
@@ -11,16 +11,14 @@ public class Main {
 		int num1 = (int) Double.parseDouble(in.next());
 
 		int tab[][] = new int[num1][0];
-		int licznik = 0;
 
-		for (int i = 0; i < num1; i++) {
-			tab[i] = new int[i + 1];
-		}
 		for (int i = 0; i < tab.length; i++) {
+			tab[i] = new int[i + 1];
 			for (int j = 0; j < tab[i].length; j++) {
 				tab[i][j] = j;
 			}
 		}
+
 		for (int i = 0; i < tab.length; i++) {
 			for (int j = 0; j < tab[i].length; j++)
 				System.out.print(tab[i][j] + " ");
--- Wynagrodzenie/src/ppj/git/Main.java
+++ Wynagrodzenie/src/ppj/git/Main.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Scanner in = new Scanner (System.in);
-		
+
+		Scanner in = new Scanner(System.in);
+
 		System.out.print("Podaj ilosc przepracownanych godzin:  ");
 		double godziny1 = Double.parseDouble(in.next());
-		
-		System.out.print("Podaj stawke:  ");
-		double stawka1 = Double.parseDouble(in.next());
-		
-		if (godziny1<=40)
-		{
-			System.out.printf("Wynagrodzenie =%8.2f",godziny1*stawka1);	
+
+		if (godziny1 > 60) {
+
+			throw new IllegalArgumentException("Maxymalnie 60 godziny pracy na tydzien: " + godziny1);
 		}
-		
-		else if (godziny1>=60)
-		{
-			System.out.printf("Wynagrodzenie =%8.2f",((40*stawka1)+(20*(stawka1*1.5))));
-			System.out.println(" Maksymalnie 60 godzin pracy na tydzien ");
-			}
-		
-		else {
-			System.out.printf("Wynagrodzenie =%8.2f",((40*stawka1)+((godziny1-40)*(stawka1*1.5))));
+
+		System.out.print("Podaj stawke za godzine pracy:  ");
+		double stawka1 = Double.parseDouble(in.next());
+
+		if (stawka1 <= 0) {
+
+			throw new IllegalArgumentException("Stawka za godzine musi byc wieksza od 0: " + stawka1);
+		}
+
+		if (godziny1 <= 40) {
+			System.out.printf("Wynagrodzenie =%8.2f", godziny1 * stawka1);
+		}
+
+		else if (godziny1 >= 60) {
+			System.out.printf("Wynagrodzenie =%8.2f", ((40 * stawka1) + (20 * (stawka1 * 1.5))));
 		}
 	}
 }
